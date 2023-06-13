cd0665d1be6c5d97b08259f4ec9a06123774abc6
mehedihasanrupok <hasanmehedi4114@gmail.com>
2023-06-14 00:39:35 +0600

    Response

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const port = process.env.PORT || 5000;
 
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+
+app.get('/', (req,res) =>{
+    res.send('Boss is Sitting')
+})
+
+app.listen(port, () =>{
+    console.log(`Bistro boss is sitting on port ${port}`);
+})
